b2bade310ff6807e9e9be5160173c893b957e1ba
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-08-31 16:56:16 -0400

    commit tree 9ec23ea4ac3356f6ef78ac9ecbd30dfc5681cfd8

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
+    attr_reader :discount, :total
+    attr_accessor :discount, :total
 
+    def initialize (discount=0, total=0)
+        @discount =discount
+        @total=total
+    end
+    def add_item(title, price, quantity=1)
+        @price = price
+        @total += price * quantity
+    end
+    def apply_discount
+        if (discount !=0)
+        self.total =(total * ((100.0 - discount.to_f)/100))
+        "After the discount, the total comes to $#{self.total}."
+        else "There is no discount to apply."
+        end
+    end
+
+
+
+
+
+end
